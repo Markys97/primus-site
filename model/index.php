79f51d18b10d9d87bd62c257cea0df1dfe21ac82
Markys97 <70927348+Markys97@users.noreply.github.com>
2022-08-01 18:05:42 +0300

    Add files via upload

---
+++ model/index.php
@@ -0,0 +1,13 @@
+<?php
+require_once '../config/database.php';
+
+$query=' SELECT * FROM user';
+
+$req= $db->prepare($query);
+$req->execute();
+
+$users= $req->fetchAll();
+
+
+
+echo sizeof($users);
